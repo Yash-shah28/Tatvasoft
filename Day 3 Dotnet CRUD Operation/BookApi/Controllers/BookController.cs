1e7e5fe0b893ec62bff11f4470055d391addb323
yash shah <139542346+Yash-shah28@users.noreply.github.com>
2025-05-29 16:40:01 +0530

    Day 3

---
+++ Day 3 Dotnet CRUD Operation/BookApi/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using BookApi.Model;
+using BookApi.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController: Controller
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService book)
+        {
+            _bookService = book;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public ActionResult AddBook(Book book)
+        {
+            _bookService.AddBook(book);
+            return Ok("Book created !");
+        }
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult GetAll()
+        {
+            return Ok(_bookService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+
+        public ActionResult GetById(int id)
+        {
+            var res = this._bookService.GetBookById(id);
+
+            if (res != null) { return Ok(res); }
+
+            return NotFound("Book not found!");
+        }
+
+
+    }
+}
---
+++ Day 3 Dotnet CRUD Operation/BookApi/Service/BookService.cs
@@ -0,0 +1,31 @@
+﻿using BookApi.Model;
+
+namespace BookApi.Service
+{
+    public class BookService
+    {
+        private List<Book> _books;
+
+        public BookService()
+        {
+            _books = new List<Book>();
+        }
+
+        // To Add Book
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        // To Get All Books
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public Book? GetBookById(int id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+    }
+}
