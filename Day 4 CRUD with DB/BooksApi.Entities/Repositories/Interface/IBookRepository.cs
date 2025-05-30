1301c210c4ce328194cc06ff2e838c977506d48f
Yash-shah28 <yashshah28072004@gmail.com>
2025-05-30 16:07:27 +0530

    Day 4

---
+++ Day 4 CRUD with DB/BooksApi.Entities/Repositories/Interface/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooksApi.Entities.Entities;
+
+namespace BooksApi.Entities.Repositories.Interface
+{
+    public interface IBookRepository
+    {
+        Task InsertBook(BookDetails bookDetails);
+        BookDetails GetById(int id);
+        Task<List<BookDetails>> GetAllAsync();
+        Task UpdateBook(BookDetails bookDetails);
+        Task DeleteBook(int id);
+    }
+}
